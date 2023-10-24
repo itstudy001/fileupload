35bd6521855efda9c36d0e86ad063ebf46698263
itstudy001 <itstudy@kakao.com>
2023-10-25 08:16:19 +0900

    initial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React ,{useState} from 'react';
+import AWS from 'aws-sdk'
+
+const S3_BUCKET ='itstudybucket';
+const REGION ='ap-northeast-1';
+
+AWS.config.update({
+    accessKeyId: 'AKIA264FV5MWPNFTR67R',
+    secretAccessKey: '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'
+})
+
+const myBucket = new AWS.S3({
+    params: { Bucket: S3_BUCKET},
+    region: REGION,
+})
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [progress , setProgress] = useState(0);
+    const [selectedFile, setSelectedFile] = useState(null);
+
+    const handleFileInput = (e) => {
+        setSelectedFile(e.target.files[0]);
+    }
+
+    const uploadFile = (file) => {
+
+        const params = {
+            ACL: 'public-read',
+            Body: file,
+            Bucket: S3_BUCKET,
+            Key: file.name
+        };
+        myBucket.putObject(params)
+        .on('httpUploadProgress', (evt) => {
+            setProgress(Math.round((evt.loaded / evt.total) * 100))
+        })
+        .send((err) => {
+            if (err) console.log(err)
+        })
+}
+
+return <div>
+    <div>Native SDK File Upload Progress is {progress}%</div>
+    <input type="file" onChange={handleFileInput}/>
+    <button onClick={() => uploadFile(selectedFile)}> Upload to S3</button>
+</div>
 }
 
 export default App;
